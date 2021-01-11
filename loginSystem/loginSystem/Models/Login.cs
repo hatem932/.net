813fa0617a955315a089fc0949b78d793e494a83
hatem932 <37874744+hatem932@users.noreply.github.com>
2021-01-11 14:34:30 +0200

    Add files via upload

---
+++ loginSystem/loginSystem/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace loginSystem.Models
+{
+    public class Login
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Login>(this);
+        
+    }
+}
